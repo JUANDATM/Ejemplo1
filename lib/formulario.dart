896a9f2eeb029e7b3613fac127279ae2859fad7f
juan daniel torres moreno <kira00711@hotmail.com>
2019-10-23 16:29:38 -0500

    first commit

---
+++ lib/formulario.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class RegistroPage extends StatefulWidget {
+  RegistroPage({Key key}) : super(key: key);
+
+  @override
+  _RegistroPageState createState() => _RegistroPageState();
+}
+
+class _RegistroPageState extends State<RegistroPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Nuevo Alumno"),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: Form(
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(
+                    filled: true,
+                      icon: Icon(Icons.person),
+                      labelText: "Nombre",
+                      hintText: "Ingresa Tu Nombre"),
+                ),
+                SizedBox(height: 15),
+                TextFormField(
+                  decoration: InputDecoration(
+                    filled: true,
+                      icon: Icon(Icons.format_list_numbered),
+                      labelText: "No Control",
+                      hintText: "Ingresa Tu Numero de control"),
+                ),
+                SizedBox(height: 15),
+                TextFormField(
+                  decoration: InputDecoration(
+                    filled: true,
+                      icon: Icon(Icons.people),
+                      labelText: "Carrera",
+                      hintText: "Ingresa Tu Carrera"),
+                ),
+                SizedBox(height: 15),
+                MaterialButton(
+                  child: Text("Registrar", style: TextStyle(color: Colors.white),),
+                  onPressed: (){},
+                  color: Colors.blue,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:ejemplo1/formulario.dart';
+import 'package:ejemplo1/home.dart';
+import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        'registro':(BuildContext context)=> RegistroPage(),
+        'home' : (BuildContext context)=> HomePage(),
+      },
+      initialRoute: 'home',
+      title: 'Material App',
+    );
+  }
+}
